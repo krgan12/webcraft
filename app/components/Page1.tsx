f16fc1572de929e714dfd4725df3e8a2deada6a0
keaganrodrigues <rodrikea@shernet.sheridancollege.ca>
2025-06-09 14:05:52 -0400

    P1 First Layout

--- app/components/Page1.tsx
+++ app/components/Page1.tsx
@@ -35,7 +35,7 @@ export default function Page1() {
                 </button>
                 &nbsp;&nbsp;&nbsp;
                  <button 
-                    className="mt-8 px-4 py-2 border-red bg-white text-black rounded hover:border-red cursor-pointer transition" 
+                    className="mt-8 px-4 py-2 border-[#000000] border bg-white text-black rounded hover:border-red cursor-pointer transition" 
                     onClick={() => router.push('/about')}
                 >
                     View Our Work
