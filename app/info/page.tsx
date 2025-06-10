1e181e82238e63fcd471644edc17f382acf1d618
keaganrodrigues <rodrikea@shernet.sheridancollege.ca>
2025-06-10 16:03:02 -0400

    First part

--- app/info/page.tsx
+++ app/info/page.tsx
@@ -3,9 +3,11 @@ import { FaRegCalendarAlt, FaEnvelope, FaPhone, FaCode } from 'react-icons/fa';
 
 export default function Contact() {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col justify-between bg-[#151c26] text-[#e5eaf1]">
       {/* Header Section */}
-     
+      <div className="flex flex-col items-center justify-center flex-1 pt-24 pb-16">
+        <h1 className="text-4xl md:text-5xl font-semibold mb-6 text-center">Ready to Get Started?</h1>
+      </div>
       {/* Footer Section */}
 
       {/* Logo & Description */}
@@ -15,7 +17,7 @@ export default function Contact() {
        {/* Company */}
 
        {/* Contact */}
-             
+
     </div>
   );
 }
