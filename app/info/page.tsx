be3db4be193e9325115b8878de0149553903a82a
keaganrodrigues <rodrikea@shernet.sheridancollege.ca>
2025-06-10 16:03:50 -0400

    Buttons Added. More UI Additions

--- app/info/page.tsx
+++ app/info/page.tsx
@@ -8,6 +8,13 @@ export default function Contact() {
       <div className="flex flex-col items-center justify-center flex-1 pt-24 pb-16">
         <h1 className="text-4xl md:text-5xl font-semibold mb-6 text-center">Ready to Get Started?</h1>
       </div>
+      <p className="text-lg md:text-xl text-[#bfc9d6] text-center mb-2 max-w-2xl">Let's discuss your project and create a website that helps your business grow.<br />Book a free consultation today.</p>
+        <div className="flex gap-6 mt-8">
+          <button className="flex items-center gap-2 bg-white text-[#151c26] font-medium px-6 py-3 rounded-lg shadow hover:bg-[#e5eaf1] transition">
+            <FaRegCalendarAlt className="text-xl" />
+            Book Free Call
+          </button>
+      </div>
       {/* Footer Section */}
 
       {/* Logo & Description */}
