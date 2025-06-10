f169dab26d03fc9a6da9583dc86794d3f6d5d663
keaganrodrigues <rodrikea@shernet.sheridancollege.ca>
2025-06-10 16:01:03 -0400

    Structure for Info page Completed

--- app/info/page.tsx
+++ app/info/page.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { FaRegCalendarAlt, FaEnvelope, FaPhone, FaCode } from 'react-icons/fa';
 
 export default function Contact() {
   return (
-    <div>Info</div>
-  )
+    <div>
+      {/* Header Section */}
+     
+      {/* Footer Section */}
+
+      {/* Logo & Description */}
+
+       {/* Services */}
+
+       {/* Company */}
+
+       {/* Contact */}
+             
+    </div>
+  );
 }
