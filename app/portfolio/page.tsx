2e86c5205ddecd4ec1b80a0a0623f43caa496ff4
keaganrodrigues <rodrikea@shernet.sheridancollege.ca>
2025-06-10 10:02:27 -0400

    Portfolio Added

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -2,6 +2,20 @@ import React from 'react'
 
 export default function Portfolio() {
   return (
-    <div>Portfolio</div>
+    <div>
+      <h1>Our Recent Work</h1>
+      <p>Take a look at some of the websites we've create for our clients</p>
+
+      { /* Project Section Cards*/}
+      <div>
+        Website 1
+      </div>
+      <div>
+        Website 2
+      </div>
+      <div>
+        Website 3
+      </div>
+    </div>
   )
 }
