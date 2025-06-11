36bcd1ba905561f2561b47c6c7a16c9ed6775327
keaganrodrigues <rodrikea@shernet.sheridancollege.ca>
2025-06-11 01:34:01 -0400

    FAQ Title Made

--- app/FAQ/page.tsx
+++ app/FAQ/page.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function FAQ() {
   return (
-    <div>FAQ</div>
+    <div className='min-h-screen p-10'>
+      <h2 className="text-4xl font-bold mb-8">
+        FAQ
+      </h2>
+    </div>
   )
 }
