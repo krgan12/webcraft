5f3c42a03ca2a0148fc1849689410e1b82e47af9
keaganrodrigues <rodrikea@shernet.sheridancollege.ca>
2025-06-21 14:03:05 -0400

    Scrollable Sections

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,23 +5,34 @@ import React from 'react'
 import { FaCode } from 'react-icons/fa'
 
 export default function Navbar() {
+  const scrollToSection = (sectionId: string) => {
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
     <nav className="bg-white text-black p-4 flex items-center justify-between relative">
-      <Link href="/" className='flex items-center space-x-2 ml-20'>
+      <div 
+        onClick={() => scrollToSection('home')} 
+        className='flex items-center space-x-2 ml-20 cursor-pointer'
+      >
         <div className='bg-[#1c2331] rounded-lg p-2'>
           <FaCode className='text-white text-xl'/>
         </div>
         <div className='text-xl font-bold'>WebCraft</div>
-      </Link>
+      </div>
         {/* <div className="space-x-4"> */}
         <div className="absolute left-1/2 transform -translate-x-1/2 flex space-x-4">
-            <Link href="/" className="hover:underline">Home</Link>
-            <Link href="/services" className="hover:underline">Services</Link>
-            <Link  href="/whyus" className="hover:underline">Why Us?</Link>
-            <Link href="/portfolio" className="hover:underline">Portfolio</Link>
-            <Link href="/custreviews" className="hover:underline">Client Reviews</Link>
-            <Link href="/FAQ" className="hover:underline">FAQ</Link>
-            <Link href="/booking" className="hover:underline">Book a Free Call</Link>
+            <Link href="/" className='hover:underline cursor-pointer'>Home</Link>
+            {/* <button onClick={() => scrollToSection('home')} className="hover:underline transition-colors cursor-pointer">Home</button> */}
+            <button onClick={() => scrollToSection('services')} className="hover:underline transition-colors cursor-pointer">Services</button>
+            <button onClick={() => scrollToSection('whyus')} className="hover:underline transition-colors cursor-pointer">Why Us?</button>
+            <button onClick={() => scrollToSection('portfolio')} className="hover:underline transition-colors cursor-pointer">Portfolio</button>
+            <button onClick={() => scrollToSection('customer')} className="hover:underline transition-colors cursor-pointer">Client Reviews</button>
+            <button onClick={() => scrollToSection('faq')} className="hover:underline transition-colors cursor-pointer">FAQ</button>
+            <Link href="/booking" className="hover:text-gray-600 transition-colors">Book a Free Call</Link>
         </div>
     </nav>
   )
